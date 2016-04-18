9693986dcf1b0dadc9d4dd81237f6100587d9e06
Scott Willeke <scott@willeke.com>
2016-04-17 23:52:36 -0700

    more comprehensive example of using the api with get sheet

--- src/server/app.js
+++ src/server/app.js
@@ -1,14 +1,36 @@
+
+/** 
+ * Calls the Smartsheet API using the specified URL and options.
+ * Authentication from the current user's OAuth access token is automatically used.
+ * @param {string} url The url to call.
+ * @param {object} options Options compatible with URLFetchApp.fetch. See https://developers.google.com/apps-script/reference/url-fetch/url-fetch-app#fetchurl-params
+ * @return {object} Parsed JSON result of the response. 
+ */
+function callSmartsheet(url, options) {
+  var localOptions = {
+    'method': 'get'
+    , 'headers': {'Authorization': 'Bearer ' + getSmartsheetAccessToken() }
+  };
+  localOptions = _.extend(localOptions, options);
+  var response = UrlFetchApp.fetch(url, localOptions).getContentText(); 
+  return JSON.parse(response);
+}
+
 /**
  * An example of making a call to the Smartsheet API.
  * In this case, gets information about the currently logged in user to Smartsheet. See http://smartsheet-platform.github.io/api-docs/#get-current-user 
  */
 function getSmartsheetUser() {
   var url = 'https://api.smartsheet.com/2.0/users/me';
-  var options = {
-    'method': 'get'
-    , 'headers': {'Authorization': 'Bearer ' + getSmartsheetAccessToken() }
-  };
-  var response = UrlFetchApp.fetch(url, options).getContentText();
-  //Logger.log('email:%s',JSON.parse(response).email); 
-  return JSON.parse(response);
+  return callSmartsheet(url);
+}
+
+function getSheets() {
+  var url = 'https://api.smartsheet.com/2.0/sheets?includeAll=true';
+  return callSmartsheet(url);
+}
+
+function getSheet(sheetId) {
+  var url = 'https://api.smartsheet.com/2.0/sheets/' + sheetId;
+  return callSmartsheet(url);
 }
