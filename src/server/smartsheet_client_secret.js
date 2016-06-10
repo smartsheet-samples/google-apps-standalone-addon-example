03709eb80388070bdec327d7e3c6a7abc5b92829
Scott McAllister <stmcallister@gmail.com>
2016-06-10 13:25:53 -0700

    saving client info for local demo branch

--- src/server/smartsheet_client_secret.js
+++ src/server/smartsheet_client_secret.js
@@ -1,3 +1,3 @@
 /* Visit http://smartsheet-platform.github.io/api-docs/#third-party-app-development to get these */
-var SMARTSHEET_CLIENT_ID = '<YOUR_CLIENT_ID>';
-var SMARTSHEET_CLIENT_SECRET = '<YOUR_CLIENT_SECRET>';
+var SMARTSHEET_CLIENT_ID = '1f914lx7fzhxtrqpvxb';
+var SMARTSHEET_CLIENT_SECRET = '137d1pz8rhivh85237m';
