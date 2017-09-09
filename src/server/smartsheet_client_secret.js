adb36ddddf28a48fd81b02e5d53a90d3c743cb34
Scott McAllister <stmcallister@gmail.com>
2017-09-09 00:12:24 -0700

    refactoring code for using gas

--- src/server/smartsheet_client_secret.js
+++ src/server/smartsheet_client_secret.js
@@ -1,3 +1,4 @@
 /* Visit http://smartsheet-platform.github.io/api-docs/#third-party-app-development to get these */
-var SMARTSHEET_CLIENT_ID = '1f914lx7fzhxtrqpvxb';
-var SMARTSHEET_CLIENT_SECRET = '137d1pz8rhivh85237m';
+var SMARTSHEET_CLIENT_ID = '1gm7whseojm7rulfp11';
+var SMARTSHEET_CLIENT_SECRET = '1hnr2vzcmxtavqblloz';
+
